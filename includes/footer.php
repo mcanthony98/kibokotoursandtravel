9af030c45502b0a492e77c6bdccd4a5084249591
Tonny G <118627853+tonnygitonga@users.noreply.github.com>
2024-03-17 19:13:24 +0300

    Added universal footer code

---
+++ includes/footer.php
@@ -0,0 +1,168 @@
+    <!-- ======================= Footer START -->
+
+<footer class="bg-dark pt-5">
+        <div class="container">
+            <!-- Row START -->
+            <div class="row g-4">
+
+                <!-- Widget 1 START -->
+                <div class="col-lg-3">
+                    <!-- logo -->
+                    <a href="index.html">
+                        <img class="h-40px" src="assets/images/kiboko-logo.png" alt="logo">
+                    </a>
+                    <p class="my-3 text-muted">Kiboko Tours & Travel.</p>
+                    <p class="mb-2"><a href="kibokotoursandtravel.com" class="text-muted text-primary-hover"><i
+                                class="bi bi-telephone me-2"></i>+254 712 123 123</a> </p>
+                    <p class="mb-0"><a href="mailto:info@kibokotoursandtravel.com"
+                            class="text-muted text-primary-hover"><i
+                                class="bi bi-envelope me-2"></i>info@kibokotoursandtravel.com</a></p>
+                </div>
+                <!-- Widget 1 END -->
+
+                <!-- Widget 2 START -->
+                <div class="col-lg-8 ms-auto">
+                    <div class="row g-4">
+                        <!-- Link block -->
+                        <div class="col-6 col-md-3">
+                            <h5 class="text-white mb-2 mb-md-4">Page</h5>
+                            <ul class="nav flex-column text-primary-hover">
+                                <li class="nav-item"><a class="nav-link text-muted" href="index.html">Home</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="#">Safaris</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="about-us.html">About us</a>
+                                </li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="contact-us.html">Contact
+                                        us</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="blog.html">Blog</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="quote.html">Quote</a></li>
+                            </ul>
+                        </div>
+
+                        <!-- Link block -->
+                        <div class="col-6 col-md-3">
+                            <h5 class="text-white mb-2 mb-md-4">Link</h5>
+                            <ul class="nav flex-column text-primary-hover">
+                                <li class="nav-item"><a class="nav-link text-muted" href="login.html">Login</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="login.html">Register</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="#">Privacy Policy</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="#">Terms</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="#">Cookie</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="#">Support</a></li>
+                            </ul>
+                        </div>
+
+                        <!-- Link block -->
+                        <div class="col-6 col-md-3">
+                            <h5 class="text-white mb-2 mb-md-4">Safaris</h5>
+                            <ul class="nav flex-column text-primary-hover">
+                                <li class="nav-item"><a class="nav-link text-muted" href="classic-safaris.html">Classic
+                                        Safaris</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted"
+                                        href="accessible-safaris.html">Accessible Safaris</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted"
+                                        href="family-couple-safaris.html">Family & Couple Safaris</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Link block -->
+                        <div class="col-6 col-md-3">
+                            <h5 class="text-white mb-2 mb-md-4">Extra Services</h5>
+                            <ul class="nav flex-column text-primary-hover">
+                                <li class="nav-item"><a class="nav-link text-muted" href="#"><i
+                                            class="fa-solid fa-hotel me-2"></i>Hotel Booking</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="#"><i
+                                            class="fa-solid fa-plane me-2"></i>Flight Booking</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="#"><i
+                                            class="fa-solid fa-globe-americas me-2"></i>Personlized Tours</a></li>
+                                <li class="nav-item"><a class="nav-link text-muted" href="#"><i
+                                            class="fa-solid fa-car me-2"></i>Cabs Booking</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- Widget 2 END -->
+
+            </div><!-- Row END -->
+
+
+            <!-- Payment and card -->
+            <div class="row g-4 justify-content-between mt-0 mt-md-2">
+
+                <!-- Payment card -->
+                <div class="col-sm-7 col-md-6 col-lg-4">
+                    <h5 class="text-white mb-2">Payment & Security</h5>
+                    <ul class="list-inline mb-0 mt-3">
+                        <li class="list-inline-item"> <a href="#"><img src="assets/images/element/paypal.svg"
+                                    class="h-30px" alt=""></a></li>
+                        <li class="list-inline-item"> <a href="#"><img src="assets/images/element/visa.svg"
+                                    class="h-30px" alt=""></a></li>
+                        <li class="list-inline-item"> <a href="#"><img src="assets/images/element/mastercard.svg"
+                                    class="h-30px" alt=""></a></li>
+                        <li class="list-inline-item"> <a href="#"><img src="assets/images/element/expresscard.svg"
+                                    class="h-30px" alt=""></a></li>
+                    </ul>
+                </div>
+
+                <!-- Social media icon -->
+                <div class="col-sm-5 col-md-6 col-lg-3 text-sm-end">
+                    <h5 class="text-white mb-2">Follow us on</h5>
+                    <ul class="list-inline mb-0 mt-3">
+                        <li class="list-inline-item"> <a class="btn btn-sm px-2 bg-facebook mb-0" href="#"><i
+                                    class="fab fa-fw fa-facebook-f"></i></a> </li>
+                        <li class="list-inline-item"> <a class="btn btn-sm shadow px-2 bg-instagram mb-0" href="#"><i
+                                    class="fab fa-fw fa-instagram"></i></a> </li>
+                        <li class="list-inline-item"> <a class="btn btn-sm shadow px-2 bg-twitter mb-0" href="#"><i
+                                    class="fab fa-fw fa-twitter"></i></a> </li>
+                        <li class="list-inline-item"> <a class="btn btn-sm shadow px-2 bg-linkedin mb-0" href="#"><i
+                                    class="fab fa-fw fa-linkedin-in"></i></a> </li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- Divider -->
+            <hr class="mt-4 mb-0">
+
+            <!-- Bottom footer -->
+            <div class="row">
+                <div class="container">
+                    <div class="d-lg-flex justify-content-between align-items-center py-3 text-center text-lg-start">
+                        <!-- copyright text -->
+                        <div class="text-muted text-primary-hover"> Copyrights ©2024 Kiboko Tours & Travel. Built by <a
+                                href="https://www.ganiamtech.com/index.php" class="text-muted">Ganiam Tech</a>. </div>
+                        <!-- copyright links-->
+                        <div class="nav mt-2 mt-lg-0">
+                            <ul class="list-inline text-primary-hover mx-auto mb-0">
+                                <li class="list-inline-item me-0"><a class="nav-link py-1 text-muted" href="#">Privacy
+                                        policy</a></li>
+                                <li class="list-inline-item me-0"><a class="nav-link py-1 text-muted" href="#">Terms and
+                                        conditions</a></li>
+                                <li class="list-inline-item me-0"><a class="nav-link py-1 text-muted pe-0"
+                                        href="#">Refund policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- =======================Footer END -->
+
+
+    <!-- Back to top -->
+    <div class="back-top"></div>
+
+    <!-- Bootstrap JS -->
+    <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Vendors -->
+    <script src="assets/vendor/tiny-slider/tiny-slider.js"></script>
+    <script src="assets/vendor/purecounterjs/dist/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.js"></script>
+    <script src="assets/vendor/flatpickr/js/flatpickr.min.js"></script>
+    <script src="assets/vendor/choices/js/choices.min.js"></script>
+    <script src="assets/vendor/jarallax/jarallax.min.js"></script>
+    <script src="assets/vendor/jarallax/jarallax-video.min.js"></script>
+
+    <!-- ThemeFunctions -->
+    <script src="assets/js/functions.js"></script>
