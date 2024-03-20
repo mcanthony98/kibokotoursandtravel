3f5b33c851c42b45a593af93403204c98cca76b7
Tonny G <118627853+tonnygitonga@users.noreply.github.com>
2024-03-21 00:23:08 +0300

    Added Payment Page Main UI Elements

--- payment.php
+++ payment.php
@@ -24,6 +24,178 @@
 
         <main>
 
+            	<!-- =======================
+Main Content START -->
+<section class="pt-4 pt-lg-5">
+	<div class="container">
+
+		<div class="row g-4 g-xl-5">
+			<!-- Left Content START -->
+			<div class="col-xl-8">
+				<div class="card bg-transparent p-0">
+					<!-- Card header START -->
+					<div class="card-header bg-transparent p-0">
+						<h1 class="card-title fs-2 mb-0">Enter Your Payment Details</h1>
+					</div>
+					<!-- Card header END -->
+
+					<!-- Card body START -->
+					<div class="card-body p-0 mt-3">
+						<!-- Alert box -->
+						<div class="alert alert-success" role="alert">
+							Hey' you are saving<strong class="mx-1">$2,560</strong>discount using coupon code
+						</div>
+
+						<form class="bg-light rounded-3 p-4">
+							<!-- Card options -->
+							<div class="d-sm-flex justify-content-sm-between align-items-center mb-3">
+								<h6 class="mb-2 mb-sm-0">We Accept:</h6>
+								<ul class="list-inline mb-0">
+									<li class="list-inline-item"> <a href="#"><img src="assets/images/element/visa.svg" class="h-30px" alt=""></a></li>
+									<li class="list-inline-item"> <a href="#"><img src="assets/images/element/mastercard.svg" class="h-30px" alt=""></a></li>
+									<li class="list-inline-item"> <a href="#"><img src="assets/images/element/expresscard.svg" class="h-30px" alt=""></a></li>
+								</ul>
+							</div>
+
+							<!-- Form START -->
+							<div class="row g-3">
+								<!-- Card number -->
+								<div class="col-12">
+									<label class="form-label"><span class="h6 fw-normal">Card Number *</span></label>
+									<div class="position-relative">
+										<input type="text" class="form-control" maxlength="14" placeholder="XXXX XXXX XXXX XXXX">
+										<img src="assets/images/element/visa.svg" class="w-30px position-absolute top-50 end-0 translate-middle-y me-2 d-none d-sm-block" alt="">
+									</div>	
+								</div>
+								<!-- Expiration Date -->
+								<div class="col-md-6">
+									<label class="form-label"><span class="h6 fw-normal">Expiration date *</span></label>
+									<div class="input-group">
+										<input type="text" class="form-control" maxlength="2" placeholder="Month">
+										<input type="text" class="form-control" maxlength="4" placeholder="Year">
+									</div>
+								</div>	
+								<!--Cvv code  -->
+								<div class="col-md-6">
+									<label class="form-label"><span class="h6 fw-normal">CVV / CVC *</span></label>
+									<input type="text" class="form-control" maxlength="3" placeholder="XXX">
+								</div>
+								<!-- Card name -->
+								<div class="col-12">
+									<label class="form-label"><span class="h6 fw-normal">Name on Card *</span></label>
+									<input type="text" class="form-control" aria-label="name of card holder" placeholder="Enter card holder name">
+								</div>
+
+								<!-- Buttons -->
+								<div class="col-12">
+									<button class="btn btn-primary w-100 mb-0">Pay Now</button>
+								</div>
+							</div>
+							<!-- Form END -->
+						</form>
+					</div>
+					<!-- Card body END -->
+				</div>
+			</div>
+			<!-- Left Content END -->
+
+			<!-- Right content START -->
+			<aside class="col-xl-4">
+				<div class="row g-4">
+					<!-- Fare summary START -->
+					<div class="col-md-6 col-xl-12">
+						<div class="card bg-light rounded-2">
+							<!-- card header -->
+							<div class="card-header border-bottom bg-light">
+								<h5 class="card-title mb-0">Fare Summary</h5>
+							</div>
+
+							<!-- Card body -->
+							<div class="card-body">
+								<ul class="list-group list-group-borderless">
+									<li class="list-group-item d-flex justify-content-between align-items-center">
+										<span class="h6 fw-normal mb-0">Base Fare
+											<a tabindex="0" data-bs-toggle="popover" data-bs-trigger="focus" data-bs-placement="bottom" data-bs-content="COVID-19 test required Vaccinated travelers can visit">
+												<i class="bi bi-info-circle"></i>
+											</a>
+										</span>
+										<span class="fs-5">$38,660</span>
+									</li>
+									<li class="list-group-item d-flex justify-content-between align-items-center">
+										<span class="h6 fw-normal mb-0">Discount</span>
+										<span class="fs-5 text-success">+$2,560</span>
+									</li>
+									<li class="list-group-item d-flex justify-content-between align-items-center">
+										<span class="h6 fw-normal mb-0">Other Services</span>
+										<span class="fs-5">$20</span>
+									</li>
+								</ul>
+							</div>
+
+							<!-- Card footer -->
+							<div class="card-footer border-top bg-light">
+								<div class="d-flex justify-content-between align-items-center">
+									<span class="h5 fw-normal mb-0">Total Fare</span>
+									<span class="h5 fw-normal mb-0">$36,500</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- Fare summary END -->
+
+					<!-- Booking START -->
+					<div class="col-md-6 col-xl-12">
+						<div class="card border">
+							<!-- Card header -->
+							<div class="card-header border-bottom">
+								<h5 class="mb-0 cardt-title">Your Booking</h5>
+							</div>
+
+							<!-- Card body -->
+							<div class="card-body">
+								<!-- Flight detail -->
+								<small><i class="bi bi-ticket me-2"></i>Flight Ticket</small>
+								<div class="d-flex mt-2">
+									<img src="assets/images/element/09.svg" class="w-40px me-2" alt="">
+									<h6 class="fw-normal mb-0">Mumbai <i class="bi bi-arrow-right"></i> New York</h6>
+								</div>
+								<ul class="nav nav-divider small text-body mt-1 mb-0">
+									<li class="nav-item">25 Jan</li>
+									<li class="nav-item">1 Stop</li>
+									<li class="nav-item">05h 25m</li>
+								</ul>
+
+								<hr> <!-- Divider -->
+
+								<!-- Traveler detail -->
+								<small><i class="bi bi-person me-1"></i>Traveler detail</small>
+								<h6 class="mb-0 fw-normal mt-2">Carolyn Ortiz</h6>
+								<ul class="nav nav-divider small text-body mt-1 mb-0">
+									<li class="nav-item">Adult</li>
+									<li class="nav-item">Female</li>
+									<li class="nav-item">Dec-2-1990</li>
+								</ul>
+							</div>
+
+							<!-- Card footer -->
+							<div class="card-footer border-top text-center p-3">
+								<a href="#" class="btn btn-link mb-0 p-0">Review booking</a>
+							</div>
+						</div>
+					</div>
+					<!-- Booking END -->
+				</div>	
+			</aside>
+			<!-- Right content END -->
+		</div>
+	</div>
+</section>
+<!-- =======================
+Main Content END -->
+
+</main>
+<!-- **************** MAIN CONTENT END **************** -->
+
         </main>
 
         <!-- Footer START -->
