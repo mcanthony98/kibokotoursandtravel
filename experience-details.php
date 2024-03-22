ecc31909bb10d4cceaca315255b235cae9a22fc3
Tonny G <118627853+tonnygitonga@users.noreply.github.com>
2024-03-22 09:04:32 +0300

    Added main UI elements

--- experience-details.php
+++ experience-details.php
@@ -24,6 +24,164 @@
 
         <main>
 
+
+        <!-- Activity Details START -->
+        <div class="card bg-transparent">
+
+            <!-- Card header -->
+            <div class="card-header border-bottom bg-transparent px-0 pt-0">
+                <h4 class="mb-0">Hot Air Balloon Rides</h4>
+            </div>
+
+            <!-- Card body -->
+            <div class="card-body pt-4 p-0">
+                <!-- List -->
+                <ul>
+                    <li class="mb-2">Experience the thrill of a lifetime with our Hot Air Balloon Rides over the breathtaking landscapes of Kenya. (Available daily from 6:00 am) </li>
+                    <li class="mb-2">Our expert pilots ensure a smooth and safe journey, making this a perfect activity for families, couples, or solo adventurers.</li>
+                    <li>For an unforgettable experience, we also offer a champagne breakfast upon landing. The total cost for the ride and breakfast is $250 per person.</li>
+                </ul>
+
+                <!-- Activity images -->
+                <h5>Hot Air Balloon Ride Images</h5>
+
+                <!-- Alert box -->
+                <div class="alert alert-warning" role="alert">
+                    Text.
+                </div>
+
+                <!-- Images -->
+                <div class="row">
+                    <!-- Slider START -->
+                    <div class="tiny-slider arrow-round arrow-xs arrow-dark">
+                        <div class="tiny-slider-inner rounded-2" data-autoplay="false" data-arrow="true" data-dots="false" data-items="3" data-items-sm="2">
+                            <!-- Image item -->
+                            <div>
+                                <a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/activities/hot-air-balloon.jpg">
+                                    <div class="card card-element-hover card-overlay-hover overflow-hidden">
+                                        <!-- Image -->
+                                        <img src="assets/images/activities/hot-air-balloon.jpg" class="rounded-3" alt="">
+                                        <!-- Full screen button -->
+                                        <div class="hover-element w-100 h-100">
+                                            <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <!-- Image item -->
+                            <div>
+                                <a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/activities/hot-air-balloon.jpg">
+                                    <div class="card card-element-hover card-overlay-hover overflow-hidden">
+                                        <!-- Image -->
+                                        <img src="assets/images/activities/hot-air-balloon.jpg" class="rounded-3" alt="">
+                                        <!-- Full screen button -->
+                                        <div class="hover-element w-100 h-100">
+                                            <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <!-- Image item -->
+                            <div>
+                                <a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/activities/hot-air-balloon.jpg">
+                                    <div class="card card-element-hover card-overlay-hover overflow-hidden">
+                                        <!-- Image -->
+                                        <img src="assets/images/activities/hot-air-balloon.jpg" class="rounded-3" alt="">
+                                        <!-- Full screen button -->
+                                        <div class="hover-element w-100 h-100">
+                                            <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <!-- Image item -->
+                            <div>
+                                <a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/activities/hot-air-balloon.jpg">
+                                    <div class="card card-element-hover card-overlay-hover overflow-hidden">
+                                        <!-- Image -->
+                                        <img src="assets/images/activities/hot-air-balloon.jpg" class="rounded-3" alt="">
+                                        <!-- Full screen button -->
+                                        <div class="hover-element w-100 h-100">
+                                            <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <!-- Image item -->
+                            <div>
+                                <a class="w-100 h-100" data-glightbox data-gallery="gallery" href="assets/images/activities/hot-air-balloon.jpg">
+                                    <div class="card card-element-hover card-overlay-hover overflow-hidden">
+                                        <!-- Image -->
+                                        <img src="assets/images/activities/hot-air-balloon.jpg" class="rounded-3" alt="">
+                                        <!-- Full screen button -->
+                                        <div class="hover-element w-100 h-100">
+                                            <i class="bi bi-fullscreen fs-6 text-white position-absolute top-50 start-50 translate-middle bg-dark rounded-1 p-2 lh-1"></i>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Slider END -->
+                </div>
+            </div>	
+
+        </div>
+        <!-- Activity Details END -->
+        
+        <!-- Inclusion & Exclusion START -->
+        <div class="card bg-transparent">
+            <!-- Card header -->
+            <div class="card-header border-bottom bg-transparent px-0 pt-0">
+                <h4 class="mb-0">Inclusion & Exclusion</h4>
+            </div>
+
+            <!-- Card body START -->
+            <div class="card-body pt-4 p-0">
+                <!-- Detail START -->
+                <div class="row g-3">
+                    <!-- List -->
+                    <div class="col-sm-6">
+                        <h5>Included in your price</h5>
+                        <ul class="list-group list-group-borderless mb-0">
+                            <li class="list-group-item mb-0 pb-0"><i class="fa-solid fa-check text-success me-1"></i>
+                                State tax
+                            </li>
+                            <li class="list-group-item mb-0 pb-0"><i class="fa-solid fa-check text-success me-1"></i>
+                                Toll charge
+                            </li>
+                            <li class="list-group-item mb-0 pb-0"><i class="fa-solid fa-check text-success me-1"></i>
+                                Driver Allowance
+                            </li>
+                            <li class="list-group-item mb-0 pb-0"><i class="fa-solid fa-check text-success me-1"></i>
+                                Only one pickup and drop
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- List -->
+                    <div class="col-sm-6">
+                        <h5>Extra charge</h5>
+                        <ul class="list-group list-group-borderless mb-0">
+                            <li class="list-group-item mb-0 pb-0"><i class="bi bi-x-lg text-danger me-1"></i>
+                                Fare beyond 600kms
+                            </li>
+                            <li class="list-group-item mb-0 pb-0"><i class="bi bi-x-lg text-danger me-1"></i>
+                                Airport entry charge
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- Detail END -->
+            </div>
+            <!-- Card body END -->
+        </div>
+        <!-- Inclusion & Exclusion END -->
+
         </main>
 
         <!-- Footer START -->
