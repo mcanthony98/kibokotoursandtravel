d61edc44a683c60084918815f00aa9e5a58ea6ac
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-04 22:04:53 +0300

    Added content to the Our Benefits section

--- index.php
+++ index.php
@@ -690,43 +690,67 @@ Our Benefits START -->
         </div> 
 
         <!-- Accordion -->
-        <div class="accordion" id="accordionExample">
+        <div class="accordion" id="accordionOurBenefits">
             <div class="accordion-item">
             <h2 class="accordion-header" id="headingOne">
                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                A variety of the best destinations in Kenya
+                <b>1.	Our tours are tailored specifically to our guests’ requirements</b>
                 </button>
             </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                <!-- Content goes here -->
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionOurBenefits">
+                    <div class="accordion-body" style="color: black;">
+                    All tours and packages are custom-made to meet the specific travel needs of each of our guests to make travel as easy, practical, and enjoyable as possible. We cover everything from custom food options to individualized activities, amending our inclusions and programs accordingly.                </div>
                 </div>
             </div>
-            </div>
+
             <div class="accordion-item">
             <h2 class="accordion-header" id="headingTwo">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Accessible Safaris to cater for everyone
+                <b>2.	We are a team of specialists</b>
                 </button>
             </h2>
-            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                <!-- Content goes here -->
-                </div>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionOurBenefits">
+                <div class="accordion-body" style="color: black;">
+                Our team is made up of passionate and experienced professionals. We are skilled and knowledgeable in therapeutic travel and are experts in our fields. As well as offering tours, we are committed to educating our local communities on disability to make travel more accessible for all.                </div>
             </div>
             </div>
+
             <div class="accordion-item">
             <h2 class="accordion-header" id="headingThree">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Family & Couple Packages are available; enjoy with your loved ones.
+                <b>3.	We provide ongoing support</b>
                 </button>
             </h2>
-            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                <div class="accordion-body">
-                <!-- Content goes here -->
-                </div>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionOurBenefits">
+                <div class="accordion-body" style="color: black;">
+                We provide our guests with all the support they could need from the point of inquiry, through to their travel home. All group tours are accompanied by a trained Therapist or Special Assistant for assistance, help, and leadership.                </div>
             </div>
             </div>
+
+            <div class="accordion-item">
+            <h2 class="accordion-header" id="headingFour">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                <b>4.	We have medical attention on hand</b>
+                </button>
+            </h2>
+            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionOurBenefits">
+                <div class="accordion-body" style="color: black;">
+                All tours include air & ground emergency medical evacuation in the unlikely event additional medical attention is required at any point during the tour. You can travel safely in the knowledge that you will always be looked after.                </div>
+            </div>
+            </div>
+
+            <div class="accordion-item">
+            <h2 class="accordion-header" id="headingFive">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                <b>5.	We handpick our partners</b>
+                </button>
+            </h2>
+            <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionOurBenefits">
+                <div class="accordion-body" style="color: black;">
+                All accommodation facilities are pre-selected and audited for accessibility. We never take our guests anywhere we haven’t been ourselves to check its suitability and we work with our partners in advance to ensure they understand what adjustments may be required.                </div>
+            </div>
+            </div>
+
         </div>
 
     </div>
