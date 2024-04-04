05fa381b865be6be23842eef6271b391c08eb9ce
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-04-05 01:11:35 +0300

    Done with FAQs page

--- faq.php
+++ faq.php
@@ -23,87 +23,157 @@
         <!-- Header END -->
 
         <main>
+
 <!-- =======================
 FAQs START -->
 
 <section class="pt-8 pt-md-5">
-
     <div class="container">
-
         <!-- Title -->
         <div class="row mb-4">
             <div class="col-12 text-center">
-                <h2>Our Benefits</h2>
+                <h2>Frequently Asked Questions</h2>
             </div>
-        </div> 
+        </div>
+        <br>
+        <br>
 
         <!-- Accordion -->
-        <div class="accordion" id="accordionOurBenefits">
+        <div class="accordion" id="accordionFAQs">
             <div class="accordion-item">
-            <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                <b>1.	Our tours are tailored specifically to our guests’ requirements</b>
-                </button>
-            </h2>
-            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionOurBenefits">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <b>What is our pricing policy?</b>
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionFAQs">
                     <div class="accordion-body" style="color: black;">
-                    All tours and packages are custom-made to meet the specific travel needs of each of our guests to make travel as easy, practical, and enjoyable as possible. We cover everything from custom food options to individualized activities, amending our inclusions and programs accordingly.                </div>
+                        Prices are quoted in United States Dollars (USD). Kiboko Tours & Travel reserves the right to adjust prices due to factors outside our control, such as fuel costs, foreign currency exchange rates, taxes, and other costs on which prices are based.
+                    </div>
                 </div>
             </div>
 
             <div class="accordion-item">
-            <h2 class="accordion-header" id="headingTwo">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                <b>2.	We are a team of specialists</b>
-                </button>
-            </h2>
-            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionOurBenefits">
-                <div class="accordion-body" style="color: black;">
-                Our team is made up of passionate and experienced professionals. We are skilled and knowledgeable in therapeutic travel and are experts in our fields. As well as offering tours, we are committed to educating our local communities on disability to make travel more accessible for all.                </div>
-            </div>
-            </div>
-
-            <div class="accordion-item">
-            <h2 class="accordion-header" id="headingThree">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                <b>3.	We provide ongoing support</b>
-                </button>
-            </h2>
-            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionOurBenefits">
-                <div class="accordion-body" style="color: black;">
-                We provide our guests with all the support they could need from the point of inquiry, through to their travel home. All group tours are accompanied by a trained Therapist or Special Assistant for assistance, help, and leadership.                </div>
-            </div>
+                <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        <b>How do I book a tour with Kiboko Tours and Travel?</b>
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionFAQs">
+                    <div class="accordion-body" style="color: black;">
+                        On the date we has received your booking form and/or a deposit as outlined in the payment policy, and/or full payment, you acknowledge that you have read and understood the booking conditions, and that you have entered into a legally binding contract with Kiboko Tours & Travel.
+                    </div>
+                </div>
             </div>
 
             <div class="accordion-item">
-            <h2 class="accordion-header" id="headingFour">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                <b>4.	We have medical attention on hand</b>
-                </button>
-            </h2>
-            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionOurBenefits">
-                <div class="accordion-body" style="color: black;">
-                All tours include air & ground emergency medical evacuation in the unlikely event additional medical attention is required at any point during the tour. You can travel safely in the knowledge that you will always be looked after.                </div>
-            </div>
+                <h2 class="accordion-header" id="headingThree">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        <b>What is our payment policy?</b>
+                    </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionFAQs">
+                    <div class="accordion-body" style="color: black;">
+                        A 50% deposit is payable on confirmation of booking, and the balance is due 30 days prior to the arrival of the clients. Bookings not secured by the appropriate deposit will be released automatically and without prior notification.
+                    </div>
+                </div>
             </div>
 
             <div class="accordion-item">
-            <h2 class="accordion-header" id="headingFive">
-                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                <b>5.	We handpick our partners</b>
-                </button>
-            </h2>
-            <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionOurBenefits">
-                <div class="accordion-body" style="color: black;">
-                All accommodation facilities are pre-selected and audited for accessibility. We never take our guests anywhere we haven’t been ourselves to check its suitability and we work with our partners in advance to ensure they understand what adjustments may be required.                </div>
-            </div>
+                <h2 class="accordion-header" id="headingFour">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        <b>Can I make amendments to my booking?</b>
+                    </button>
+                </h2>
+                <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionFAQs">
+                    <div class="accordion-body" style="color: black;">
+                        Any amendment requests of confirmed elements of a booking received are subject to charges levied by our suppliers. No amendments are permitted to your booking within 30 days of departure.
+                    </div>
+                </div>
             </div>
 
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingFive">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                        <b>What is our cancellation policy?</b>
+                    </button>
+                </h2>
+                <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionFAQs">
+                    <div class="accordion-body" style="color: black;">
+                        If you wish to cancel all or part of your booking, you will need to notify Kiboko Tours & Travel in writing. Cancellation charges apply depending on the number of days before departure, ranging from 50% of the deposit to 100% of the total cost.
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSix">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                        <b>Is travel insurance required?</b>
+                    </button>
+                </h2>
+                <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionFAQs">
+                    <div class="accordion-body" style="color: black;">
+                        It is strongly recommended to have adequate travel insurance for participation on any of Kiboko Tours & Travel's tours and/or expeditions. The travel insurance policy must cover medical expenses, third-party risk, emergency repatriation, and personal liability.
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSeven">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                        <b>What are the passport and visa requirements?</b>
+                    </button>
+                </h2>
+                <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionFAQs">
+                    <div class="accordion-body" style="color: black;">
+                        It is your responsibility to carry a valid passport at all times, as well as to obtain relevant visas and/or travel permits applicable for your tour or expedition. Please ensure that your passport is valid for 6 months beyond the duration of your holiday.
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingEight">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+                        <b>What if I have a pre-existing medical condition or disability?</b>
+                    </button>
+                </h2>
+                <div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="headingEight" data-bs-parent="#accordionFAQs">
+                    <div class="accordion-body" style="color: black;">
+                        It is your responsibility to inform Kiboko Tours & Travel of any pre-existing medical conditions and/or disability that could reasonably be expected to affect the normal conduct of the tour, as well as the enjoyment of other trip members.
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingNine">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+                        <b>How do you handle complaints?</b>
+                    </button>
+                </h2>
+                <div id="collapseNine" class="accordion-collapse collapse" aria-labelledby="headingNine" data-bs-parent="#accordionFAQs">
+                    <div class="accordion-body" style="color: black;">
+                        Every effort will be made to provide you with the best holiday possible. Should you, for any reason, not be satisfied with the services provided during your stay, please inform our tour guide or member of our staff within 24 hours, so that the situation can be rectified immediately.
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTen">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
+                        <b>Can you create a custom-made vacation for me?</b>
+                    </button>
+                </h2>
+                <div id="collapseTen" class="accordion-collapse collapse" aria-labelledby="headingTen" data-bs-parent="#accordionFAQs">
+                    <div class="accordion-body" style="color: black;">
+                        Yes, if you don't see an itinerary that suits your travel style, interests, and budget on our website, please contact us, and we'll be happy to create a bespoke itinerary for you.
+                    </div>
+                </div>
+            </div>
         </div>
-
     </div>
-
 </section>
+<br>
+<br>
 
 <!-- =======================
 FAQs END -->
